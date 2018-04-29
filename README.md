c7eacc6ebac6471f6df8136ab11fdf0e2690d188
Ivan Savov <ivan.savov@gmail.com>
2018-04-29 00:29:06 -0400

    initial exploration

---
+++ README.md
@@ -0,0 +1,20 @@
+Magnutide vector embeddings for Python
+======================================
+
+
+Install
+
+    virtualenv -p python3  venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+
+Download 800MB vector model
+
+    ./bootstrap.sh
+
+Run
+
+    jupyter lab
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Downloading word feature vectors from link found in https://github.com/plasticityai/magnitude#pre-converted-magnitude-formats-of-popular-embeddings-models"
+echo "856.6 MB, so go make a coffee"
+wget http://magnitude.plasticity.ai/glove+approx/glove-lemmatized.6B.200d.magnitude
+
+
+
